034ae699a9710a7ab8b4e587b15425dcab537be0
Edoardo Putti <edoardo.putti@gmail.com>
2021-01-07 15:04:38 +0100

    Add operational semantic
    Once we have the updated syntax we can port over the original
    operational semantics from our lecture notes and update it
    to reflect the new forms.

--- ISL.v
+++ ISL.v
@@ -1,3 +1,8 @@
+(* From stdpp Require Export gmap. *)
+
+Require Export Arith String List Omega.
+Export ListNotations.
+
 Require Export map.
 
 Inductive val : Type :=
@@ -65,3 +70,138 @@ Example example_client := client (EVal (VLoc 0)).
 Check example_client.
 Print example_client.
 Compute example_client.
+
+
+(* now that the syntax is defined and I have written
+   down the examples from the paper the semantics of this
+   language is the next step; Robbert _stressed_ that we should
+   give up on the big_step semantic and just focus on the
+   small step one so that later on we can write down the Hoare
+   triples using the small step.
+ *)
+
+(* ## Semantics of operators *)
+Definition eval_bin_op (op : bin_op) (v1 v2 : val) : option val :=
+  match op, v1, v2 with
+  | PlusOp, VNat n1, VNat n2 => Some (VNat (n1 + n2))
+  | MinusOp, VNat n1, VNat n2 => Some (VNat (n1 - n2))
+  | LeOp, VNat n1, VNat n2 => Some (VBool (Nat.leb n1 n2))
+  | LtOp, VNat n1, VNat n2 => Some (VBool (Nat.ltb n1 n2))
+  | EqOp, VNat n1, VNat n2 => Some (VBool (Nat.eqb n1 n2))
+  | EqOp, VBool n1, VBool n2 => Some (VBool (Bool.eqb n1 n2))
+  | EqOp, VUnit, VUnit => Some (VBool true) (* new *)
+  | EqOp, VLoc l1, VLoc l2 => Some (VBool (Nat.eqb l1 l2)) (* new *) 
+  | _, _, _ => None
+  end.
+
+(* ## Substitution *)
+Fixpoint subst (x : string) (w : val) (e : expr) : expr :=
+  match e with
+  | EAmb => EAmb (* new *)
+  | EVal v => EVal v
+  | EVar y => if string_dec x y then EVal w else EVar y
+  | ELet y e1 e2 =>
+     if string_dec x y
+     then ELet y (subst x w e1) e2
+     else ELet y (subst x w e1) (subst x w e2)
+  | ESeq e1 e2 => ESeq (subst x w e1) (subst x w e2)
+  | EOp op e1 e2 => EOp op (subst x w e1) (subst x w e2)
+  | EIf e1 e2 e3 => EIf (subst x w e1) (subst x w e2) (subst x w e3)
+  | EWhile e1 e2 => EWhile (subst x w e1) (subst x w e2)
+  | EAlloc e => EAlloc (subst x w e)
+  | EFree e => EFree (subst x w e)
+  | ELoad e => ELoad (subst x w e)
+  | EStore e1 e2 => EStore (subst x w e1) (subst x w e2)
+  | EError => EError
+  end.
+
+(* our previous map definition was from nat to val so we might as well continue like this *)
+Notation mem := (map val).
+
+Inductive head_step : expr -> mem -> expr -> mem -> Prop :=
+  | Let_headstep m y e2 v1 :
+     head_step (ELet y (EVal v1) e2) m (subst y v1 e2) m
+  | Seq_headstep m e2 v1 :
+     head_step (ESeq (EVal v1) e2) m e2 m
+  | If_headstep_true m e2 e3 :
+     head_step (EIf (EVal (VBool true)) e2 e3) m e2 m
+  | If_headstep_false m e2 e3 :
+     head_step (EIf (EVal (VBool false)) e2 e3) m e3 m
+  | While_headstep m e1 e2 :
+     head_step (EWhile e1 e2) m (EIf e1 (ESeq e2 (EWhile e1 e2)) (EVal VUnit)) m
+  | Op_headstep m op v1 v2 v :
+     eval_bin_op op v1 v2 = Some v ->
+     head_step (EOp op (EVal v1) (EVal v2)) m (EVal v) m
+  | Alloc_headstep m v :
+     head_step (EAlloc (EVal v)) m (EVal (VLoc (mfresh m))) (minsert (mfresh m) v m)
+  | Free_headstep m l :
+     mlookup m l <> None ->
+     head_step (EFree (EVal (VLoc l))) m (EVal VUnit) (mdelete l m)
+  | Load_headstep m l v :
+     mlookup m l = Some v ->
+     head_step (ELoad (EVal (VLoc l))) m (EVal v) m
+  | Store_headstep m l v :
+     mlookup m l <> None ->
+     head_step (EStore (EVal (VLoc l)) (EVal v)) m (EVal VUnit) (minsert l v m)
+  (* new : the ambiguos expression reduces to any natural number *)
+  | Amb_headstep m (n : nat):
+     head_step EAmb m (EVal (VNat n)) m.
+
+(* there is no reduction for incorrect expressions because we want to treat
+   _stuck_ expressions as errors. This means that the **error** expression
+   will not have a reduction for example but at the same time, if you cannot
+   provide a proof that mlookup m l <> None then you cannot reduce
+   EFree (EVal (VLoc l)) as it would be an error.
+ *)
+
+Create HintDb head_step.
+Hint Constructors head_step : head_step.
+
+Inductive ctx_item : Type :=
+  | LetCtx : string -> expr -> ctx_item
+  | SeqCtx : expr -> ctx_item
+  | OpCtxL : bin_op -> expr -> ctx_item
+  | OpCtxR : bin_op -> val -> ctx_item
+  | IfCtx : expr -> expr -> ctx_item
+  | AllocCtx : ctx_item
+  | FreeCtx : ctx_item
+  | LoadCtx : ctx_item
+  | StoreCtxL : expr -> ctx_item
+  | StoreCtxR : val -> ctx_item.
+
+Notation ctx := (list ctx_item).
+
+Definition fill_item (E : ctx_item) (e : expr) : expr :=
+  match E with
+  | LetCtx s e2 => ELet s e e2
+  | SeqCtx e2 => ESeq e e2
+  | OpCtxL op e2 => EOp op e e2
+  | OpCtxR op v1 => EOp op (EVal v1) e
+  | IfCtx e2 e3 => EIf e e2 e3
+  | AllocCtx => EAlloc e
+  | FreeCtx => EFree e
+  | LoadCtx => ELoad e
+  | StoreCtxL e2 => EStore e e2
+  | StoreCtxR v1 => EStore (EVal v1) e
+  end.
+
+Fixpoint fill (E : ctx) (e : expr) : expr :=
+  match E with
+  | nil => e
+  | E1 :: E2 => fill_item E1 (fill E2 e)
+  end.
+
+Inductive step : expr -> mem -> expr -> mem -> Prop :=
+  | do_step E m1 m2 e1 e2 :
+     head_step e1 m1 e2 m2 ->
+     step (fill E e1) m1 (fill E e2) m2.
+
+Inductive steps : expr -> mem -> expr -> mem -> Prop :=
+  | steps_refl m e :
+     steps e m e m
+  | steps_step m1 m2 m3 e1 e2 e3 :
+     step e1 m1 e2 m2 ->
+     steps e2 m2 e3 m3 ->
+     steps e1 m1 e3 m3.
+
+
